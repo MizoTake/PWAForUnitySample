c0bcce7c7b554a219e15d7110ccb63ddbc4cf358
MizoTake <mizotake123@gmail.com>
2018-10-22 00:26:15 +0900

    changed

--- VueProject/pwaForUnity/config/index.js
+++ VueProject/pwaForUnity/config/index.js
@@ -8,8 +8,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '../dist/static',
-    assetsPublicPath: '../pwaForUnity',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '../',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
